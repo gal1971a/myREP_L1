5d5e092bcb5dc2a7740a3b340910844495f989a3
Galuzinsky Arthur <galnba17@gmail.com>
2020-05-26 14:01:54 -0500

    DifferentOps

--- LES_1/ConsoleApp1/ConsoleApp1/Program.cs
+++ LES_1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,12 +6,45 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class Program_Differ_Ops
     {
+        public static int c;
+        public static void Add(int a, int b)
+        {
+            c = a + b;   
+        }
+        public static double Sb(int a, int b)
+        {
+            double c = a - b; 
+            return c;
+        } 
+        public static int Ml(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Dv(int a, int b)
+        {
+            return a / b;
+        }
+        public static double Procent(double a, double procent)
+        {
+            return (a * procent)/100;
+        }
+
         static void Main(string[] args)
         {
+            Add(1, 5);
+            double cSb = Sb(4, 2);
+            int cMl = Ml(2,1);
+            int cDv = Dv(10, 1);
+            double cPr = Procent(100, 2);
             Console.WriteLine("Hi Auto TEST");
+            Console.WriteLine(cSb);
+            Console.WriteLine(cMl);
+            Console.WriteLine(cDv);
+            Console.WriteLine(cPr);
+            Console.WriteLine(c);
             Console.ReadKey();
         }
     }
-}
+ }
